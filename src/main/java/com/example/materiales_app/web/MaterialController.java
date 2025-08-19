bb7d9b39452ecb664980d479edd50c3c29df10df
RaulLopez71 <159707112+RaulLopez71@users.noreply.github.com>
2025-08-19 13:29:18 -0500

    CRUD REST de Material (#1)
    * Add .gitattributes (LF default)
    
    * Normalize line endings
    
    * CRUD REST de Material (controller con pageable)
    
    ---------
    
    Co-authored-by: Raul Lopez <ecovivienda.exquadra@gmail.com>

---
+++ src/main/java/com/example/materiales_app/web/MaterialController.java
@@ -0,0 +1,59 @@
+package com.example.materiales_app.web;
+
+
+import com.example.materiales_app.entity.Material;
+import com.example.materiales_app.repo.MaterialRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/materiales")
+public class MaterialController {
+
+    private final MaterialRepository repo;
+
+    public MaterialController(MaterialRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public Page listar(Pageable pageable) {
+        return repo.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Material> porId(@PathVariable Long id) {
+        return repo.findById(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Material> crear(@RequestBody Material m) {
+        Material creado = repo.save(m);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Material> actualizar(@PathVariable Long id, @RequestBody Material m) {
+        Optional<Material> opt = repo.findById(id);
+        if (opt.isEmpty()) return ResponseEntity.notFound().build();
+        Material db = opt.get();
+        db.setNombre(m.getNombre());
+        db.setUnidad(m.getUnidad());
+        db.setPrecio(m.getPrecio());
+        return ResponseEntity.ok(repo.save(db));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        if (!repo.existsById(id)) return ResponseEntity.notFound().build();
+        repo.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
